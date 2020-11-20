8e6d07b617edc6fc449a9a683989441c6e0df3de
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2020-11-20 10:13:38 -0500

    feat: added users table/procedure-trigger

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS dblaunchstore;
+CREATE DATABASE dblaunchstore;
+
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
@@ -24,6 +27,48 @@ CREATE TABLE "files" (
   "product_id" int
 );
 
-ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
+
+ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- Creating procedure for field updated_at
+--CREATE FUNCTION trigger_set_timestamp()
+--RETURNS TRIGGER AS 
+--$$
+--BEGIN
+--  NEW.updated_at = NOW();
+--  RETURN NEW;
+--END;
+
+--$$ LANGUAGE plpgsql;
+
+-- Creating trigger for PROCEDURE trigger_set_timestamp on PRODUCTS
+--CREATE TRIGGER set_timestamp 
+--BEFORE UPDATE ON products 
+--FOR EACH ROW 
+--EXECUTE PROCEDURE trigger_set_timestamp();
+
+
+-- Creating trigger for PROCEDURE trigger_set_timestamp on USERS
+--CREATE TRIGGER set_timestamp 
+--BEFORE UPDATE ON users 
+--FOR EACH ROW 
+--EXECUTE PROCEDURE trigger_set_timestamp();
+
+
