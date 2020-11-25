7f9aaa287fa4c4cf4143db41090d086ce67fe45e
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2020-11-25 14:25:58 -0500

    feat: added and styled user login form

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,5 +4,9 @@ logout(req, res){ // logout does not need async
    req.session.destroy() //Quando usuario clicar em SAIR no menu de Minha conta, sessao sera destruida e usuario redirecionado
 
    return res.redirect('/')
+}, 
+loginForm(req, res){
+
+   return res.render('session/index')
 }
 }
