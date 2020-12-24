6ff4c4fb176f07f63af9f537c7c16b997e51cda2
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2020-12-24 15:17:51 -0500

    refactor: Configure LoadService for Home, Product and Search Controllers

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,47 +1,17 @@
-const Product =  require("../models/Product")
-const {formatPriceComingFromDb, date} = require("../../lib/utils")
-
+const LoadProductService = require('../services/LoadProductService');
 
 module.exports = {
-
-async index(req, res) {
-
-   try {
-   const products = await Product.findAll()  
-
-   if(!products) return res.send("Products not found!")  
-
-   async function getImage(productId){
-       
-        let files = await Product.files(productId) 
-             
-         files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace('public', '').replace(/\\/g, "/")}`)
-    
-             return files[0] // he we are returning just one image
-   }
-   
-//    Para cada products usaremos a funcao getImage e para isso precisamos de usar uma cadeia de promessa
-//    Note que como abaixo nao estamos desestruturando o objeto usamos '=' e nao ':' 
-   const productsPromise = products.map(async product =>{
-    product.img = await getImage(product.id)
-    product.oldPrice = formatPriceComingFromDb(product.old_price)
-    product.price = formatPriceComingFromDb(product.price)
-    return product
-   }).filter((product, index)=> index > 2 ? false: true) // only 3 products 0, 1 , 2, 3 are
-   // Aqui estamos limitando o resultado de produtos para 2 produtos SOMENTE
-   // ou seja quando passar de 2 produtos ja retorna falso para mim
-
-
-    const lastAdded =  await Promise.all(productsPromise)
-    //console.log('linha 38', lastAdded)
-
-    return res.render('home/index', {products: lastAdded})
-
-   }
-
-   catch(err){
-      console.error(err)
-   }
-
-}
-}
+  async index(req, res) {
+    try {
+      const allProducts = await LoadProductService.load("products");
+      if (!allProducts) return res.send("Products not found!");
+      const products = allProducts.filter((product, index) =>
+        index > 2 ? false : true
+      );
+      
+      return res.render("home/index", { products });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,7 +2,8 @@ const { unlinkSync }  = require('fs')
 const Category = require("../models/Category")
 const Product =  require("../models/Product")
 const File = require("../models/File")
-const {formatPriceComingFromDb, date} = require("../../lib/utils")
+const LoadProductService = require('../services/LoadProductService')
+
 
 
 module.exports = {
@@ -66,35 +67,18 @@ module.exports = {
     }
   },
 
-  async show(req, res) {
-    const { id } = req.params;
+async show(req, res) {   
 
     try {
-      const product = await Product.find(id);
+
+      const { id } = req.params;
+      const product = await LoadProductService.load('product', {where: {id}})
+  
 
       if (!product) return res.send("Product not found!");
 
-      const { day, month, hour, minutes } = date(product.updated_at);
+      return res.render("products/show", { product});
 
-      //here we are creating an object to send to Front-end ( In te Front-end it wil be product.published.day and product.published.hour)
-      product.published = {
-        day: `${day}/${month}`,
-        hour: `${hour}h:${minutes}m`,
-      };
-
-      product.old_price = formatPriceComingFromDb(product.old_price);
-      product.price = formatPriceComingFromDb(product.price);
-
-      let files = await Product.files(product.id);
-      files = files.map((file) => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`,
-      }));
-
-      return res.render("products/show", { product, files });
     } catch (error) {
       console.error(error);
     }
@@ -102,30 +86,16 @@ module.exports = {
 
   async edit(req, res) {
     try {
-      const product = await Product.find(req.params.id);
 
+      const { id } = req.params;
+      const product = await LoadProductService.load('product', {where: {id}})
       if (!product) return res.send("Product not found!");
-
-      product.price = formatPriceComingFromDb(product.price);
-      product.old_price = formatPriceComingFromDb(product.old_price);
 
       //get categories
       const categories = await Category.findAll();
 
-      //get images-files
-      let files = await Product.files(product.id);
+      return res.render("products/edit.njk", { product, categories });
 
-      // setting the right address for the image (public\\images\\1603810102967-liveIncrivel.png')
-      files = files.map((file) => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`,
-      }));
-
-      //console.log(files)
-      return res.render("products/edit.njk", { product, categories, files });
     } catch (error) {
       console.error(error);
     }
@@ -167,7 +137,7 @@ module.exports = {
 
       if (req.body.old_price !== req.body.price) {
         const previousPrice = await Product.find(req.body.id);
-        req.body.old_price = previousPrice.rows[0].price; 
+        req.body.old_price = previousPrice.price; 
         // aqui tera que troca para previousPrice.price, pq o find ja me retorna results.rows[0]
       }
 
