5643c231ffb706b452317b1a0c886662906f2881
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-01-11 20:13:55 -0500

    feat : added add-one, remove-one and delete to the cart index page

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -6,19 +6,73 @@ module.exports = {
 
     try{
 
-        const product = await LoadProductsService.load('product', {where: { id: 1 }})
-        let { cart } = req.session // ainda nao existe o carrinho no req.session
+        let { cart } = req.session 
         // gerenciador de carrinho  cart = require("../../lib/cart")
-        cart = Cart.init(cart).addOne(product) // como ainda nao existe carrinho, ele vai retornar aquela estrutura de carrinho items = [], total
+        cart = Cart.init(cart) // aqui pegamos o carrinho da sessao e colocamos no init. Se nao tiver nada estara vazio
         
-      
          return res.render('cart/index', { cart })
 
     }catch(error){
         console.log(error)
     }
-
-
     
-}
+}, 
+
+    async addOne(req, res){
+        //gerenciador de carrinho nao deixa adicionar uma quantidade de items/produto no carrinho maior do que product.quantity 
+
+        try{
+
+            // pegar o id do produto e o produto
+            const { id } = req.params
+            const product = await LoadProductsService.load('product', { where: { id }})
+            //console.log('produto para o carriho', product)
+            // pegar o carrinho da sessao
+            let { cart } = req.session
+            //console.log('carrinho', cart)
+
+            // adicionar o produto ao carrinho (usando o gerenciador de carrinho)
+            cart = Cart.init(cart).addOne(product)
+
+            // atualizar o carrinho da Session
+            req.session.cart = cart
+
+            //redirecionar o usuario para a tela do carrinho
+            return res.redirect('/cart')
+            
+
+        }catch(error){
+
+            console.error(error)
+
+        }
+
+    }, 
+
+    removeOne(req, res){
+
+        let { id } = req.params
+
+        let { cart } = req.session
+        if(!cart) return res.redirect('/cart')
+
+        cart = Cart.init(cart).removeOne(id)
+
+        req.session.cart = cart
+
+        return res.redirect('/cart')
+
+    }, 
+
+    delete(req, res) {
+        
+        let { id } = req.params
+        let { cart } = req.session
+        if(!cart) return
+
+        req.session.cart = Cart.init(cart).delete(id)
+
+        return res.redirect('/cart')
+
+    }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.index)
-
-
+      .post('/:id/add-one', CartController.addOne) // no html colocamos o sinal de + = /cart/{{item.product.id}}/add-one
+      .post('/:id/remove-one', CartController.removeOne)
+      .post('/:id/delete', CartController.delete)
 module.exports = routes
